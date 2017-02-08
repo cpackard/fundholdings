b21ff0dbf5f975c64aadd1201583b0e01c24e2b1
Christian <chrisnpack@gmail.com>
2017-02-07 23:02:32 -0500

    Add requirements and setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    licence = f.read()
+
+setup(
+    name='holdings',
+    version='0.1',
+    description='Scrape and parse mutual fund holdings from EDGAR.'
+    long_description=readme,
+    author='Christian N Packard',
+    author_email='chrisnpack@gmail.com',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
